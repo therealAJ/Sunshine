0e0681be86921b9aa1adc8d73909e17f87060e18
Alex <alexonej@gmail.com>
2016-04-26 10:55:10 -0700

    add more dummy data

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -86,6 +86,11 @@ public class MainActivity extends ActionBarActivity {
                     "Thurs 6/26 - Rainy - 18/11",
                     "Fri 6/27 - Foggy - 21/10",
                     "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7",
+                    "Sun 6/29 - Sunny - 20/7",
+                    "Sun 6/29 - Sunny - 20/7",
+                    "Sun 6/29 - Sunny - 20/7",
+                    "Sun 6/29 - Sunny - 20/7",
                     "Sun 6/29 - Sunny - 20/7"
             };
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
